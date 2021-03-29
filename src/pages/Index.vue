135f679f9eed8d5c7a144a2bfe483d8b4c466254
Denis <Denis.silva@a.ficr.edu.br>
2021-03-28 21:42:29 -0300

    V.1
    to do list

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,72 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
+  <q-page padding>
+
+    <q-input v-model="novaTarefa" label="O que você quer fazer?" />
+
+    <q-btn outline
+      class="full-width"
+      color="primary"
+      icon="add"
+      label="Adicioniar Tarefa"
+      @click="adicionarTarefaNaLista()" />
+
+    <q-list bordered separator class="espacado-em-cima">
+
+      <q-item clickable v-ripple
+       v-for="tarefa in tarefas"
+       @click="alternarFinalizacaoDaTarefa(tarefa)">
+
+        <q-item-section
+          :class="{ 'tarefa-finalizada': tarefa.finalizada }">
+
+          {{ tarefa.descricao }}
+          </q-item-section>
+      </q-item>
+
+    </q-list>
+
+
   </q-page>
 </template>
 
+<style scoped>
+ .espacado-em-cima {
+   margin-top: 30px;
+ }
+
+ .tarefa-finalizada {
+   text-decoration: line-through;
+ }
+
+
+</style>
+
 <script>
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  data()  {
+    return {
+      novaTarefa: '',
+      tarefas: [],
+
+    };
+  },
+  methods: {
+    adicionarTarefaNaLista() {
+      const tarefa = { descricao: this.novaTarefa, finalizada: false };
+
+      this.tarefas.push(tarefa);
+
+      this.novaTarefa = '';
+    },
+
+    alternarFinalizacaoDaTarefa(tarefa) {
+      tarefa.finalizada = !tarefa.finalizada;
+
+    },
+
+  },
+
+
 }
 </script>
